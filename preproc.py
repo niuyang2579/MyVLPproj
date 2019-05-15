13f2775472db5b8e4ec3753c7a10608318104e4a
niuyang2579 <niuyang@mail.nwpu.edu.cn>
2019-05-15 20:16:37 +0800

    first commit

--- preproc.py
+++ preproc.py
@@ -1,5 +1,5 @@
 import cv2
-import pixel_array
+import numpy as np
 
 def find_range():
     img_wide = cv2.imread('single/wide/IMG_1240.JPG', 0)
@@ -20,4 +20,17 @@ def find_range():
     dx_t = f / fx_t
     dy_t = f / fy_t
 
-    H1 = pixel_array.channel()
+    m, n = img_tele.shape
+    p_t = [np.array([0, 0]), np.array([0, n-1]), np.array([m-1, 0]), np.array([m-1, n-1])]
+    p_w = []
+    #Ca, Cb是相机坐标系参数 Xc/Zc, Yc/Zc
+    for point in p_t:
+        Ca = (point[0]-x0_t) / fx_t
+        Cb = (point[1]-y0_t) / fy_t
+        x_t = Ca*fx_w + x0_w
+        y_t = Cb*fy_w + y0_w
+        p_t.append([x_t, y_t])
+    print(p_w)
+
+if __name__ == '__main__':
+    find_range()
